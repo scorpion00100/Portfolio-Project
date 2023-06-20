b111caa65da77eaf04431ebaf4d0b074ed588450
Dan Bethel IRYIVUZE <101734714+scorpion00100@users.noreply.github.com>
2023-06-20 17:29:01 +0200

    Implemented Subtraction class for subtraction operator

--- MathWiz/src/main/java/formula/Subtraction.java
+++ MathWiz/src/main/java/formula/Subtraction.java
@@ -15,7 +15,7 @@ public class Subtraction extends BinaryOperator {
     /**
      * Compute the derivative of the formula.
      *
-     * @return the derivative of the formula
+     * @return derivative of the formula
      */
     @Override
     public Formula derivative() {
